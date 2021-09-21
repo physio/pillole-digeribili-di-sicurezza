0e7457979684876f601f32a608a71e37e16c0412
physio <mauro.gallo@polin.it>
2021-09-21 21:59:08 +0200

    feat: add browser fingerprint

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,19 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+import FingerprintJS from '@fingerprintjs/fingerprintjs'
+
+// Initialize an agent at application startup.
+const fpPromise = FingerprintJS.load()
+
+;(async () => {
+  // Get the visitor identifier when you need it.
+  const fp = await fpPromise
+  const result = await fp.get()
+
+  // This is the visitor identifier:
+    const visitorId = result.visitorId
+  document.getElementById("fingerprint").value = visitorId;
+  console.log(visitorId)
+})()
