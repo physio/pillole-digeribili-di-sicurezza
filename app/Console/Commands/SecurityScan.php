a3442155896424433c92fbe9d1ab99dc36eb073f
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2021-09-21 06:42:13 +0000

    Packages scanner

---
+++ app/Console/Commands/SecurityScan.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Mail;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Enlightn\SecurityChecker\SecurityChecker;
+
+class SecurityScan extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'vulnerabilities';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check composer packages vulnerabilities';
+
+
+    public function handle(Alarms $monitors): int
+    {
+        $checker = new SecurityChecker;
+
+        $result = $checker->check(base_path('composer.lock'));
+
+        if (!count($result)) {
+            return 0;
+        }
+
+        $this->table(['Package', 'Time', 'Description'], $this->format($result));
+
+        $this->notification();
+
+        return 0;
+    }
+
+    private function format(array $result): array
+    {
+        return (collect($result)->map(function ($item, $key) {
+            return [
+                'package' => "{$key}@{$item['version']}",
+                'time' => Carbon::parse($item['time'])->format('Y:m:d'),
+                'advisories' => collect($item['advisories'])->map(function($item) {
+                    return $item['link'];
+                })->first(),
+            ];
+        }))
+        ->toArray();
+    }
+
+    private function notification(array $result): void
+    {
+        // $user = User::where('email', env('SECURITY_NOTIFICATION_EMAIL'));
+
+        // Mail::to($user)->send(new SecutityNotification($result));
+    }
+}
