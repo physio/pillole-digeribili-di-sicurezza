4a174b62440355b5951a1509bc4cfb1beed666cc
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2021-09-22 12:18:47 +0000

    Fix fingerpitn set valie

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,13 +7,19 @@ import FingerprintJS from '@fingerprintjs/fingerprintjs'
 // Initialize an agent at application startup.
 const fpPromise = FingerprintJS.load()
 
-;(async () => {
-  // Get the visitor identifier when you need it.
-  const fp = await fpPromise
-  const result = await fp.get()
+    ; (async () => {
+        // Get the visitor identifier when you need it.
+        const fp = await fpPromise
+        const result = await fp.get()
 
-  // This is the visitor identifier:
-    const visitorId = result.visitorId
-  document.getElementById("fingerprint").value = visitorId;
-  console.log(visitorId)
-})()
+        // This is the visitor identifier:
+        const visitorId = result.visitorId
+
+        let fingerprint = document.getElementById("fingerprint");
+
+        if (fingerprint) {
+            fingerprint.value = visitorId;
+        }
+
+        console.log(`Fingerprint is ${visitorId}`);
+    })()
