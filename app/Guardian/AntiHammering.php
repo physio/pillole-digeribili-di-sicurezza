ac8297693972631b7ec7e53ec22d1352555b83a7
physio <mauro.gallo@polin.it>
2021-09-23 21:03:04 +0200

    improv: better code

--- app/Guardian/AntiHammering.php
+++ app/Guardian/AntiHammering.php
@@ -9,10 +9,8 @@ class AntiHammering
 {
     public function hint(): bool
     {
-        $count = (getenv("THROTTLE_COUNT") !== false)? intval(env('THROTTLE_COUNT')) : 5;
-        $time = (getenv("THROTTLE_TIME_SPAN") !== false)? intval(env('THROTTLE_TIME_SPAN')) : 60;
 
-        Throttle::hit(Request::instance(), $count, $time);
+        Throttle::hit(Request::instance(), env('THROTTLE_COUNT', 5), env('THROTTLE_TIME_SPAN', 60));
 
         return $this->throttled();
     }
